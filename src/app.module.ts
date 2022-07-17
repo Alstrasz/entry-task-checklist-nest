c881e61950ed3b733f7d66f325ed0f00f6498ff4
Alstrasz <lutsai2000@ya.ru>
2022-07-17 20:56:43 +0300

    heroku fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,11 @@ import { TodosModule } from './todos/todos.module';
             synchronize: true,
             autoLoadEntities: true,
             ssl: process.env.POSTGRES_USE_SSL !== undefined,
+            extra: {
+                ssl: {
+                    rejectUnauthorized: process.env.POSTGRES_USE_SSL !== undefined,
+                },
+            },
         } ),
         GraphQLModule.forRoot<ApolloDriverConfig>( {
             driver: ApolloDriver,
